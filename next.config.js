ab869ac05062a2c4165f12012be5810a9fa1ca72
BlockchainLibrary-BL <hello@blockchain-library.org>
2025-07-05 15:29:01 +0100

    Add next.config.js and update package files

---
+++ next.config.js
@@ -0,0 +1,8 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  transpilePackages: ['@reown/appkit']
+};
+
+module.exports = nextConfig;
